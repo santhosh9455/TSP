3c7adfcdae69bbcf37d42bd1627cd3b1c3348d40
PriyaImman <121866079+PriyaImman@users.noreply.github.com>
2025-08-21 21:35:42 +0530

    feat: Revamp view student component with enhanced UI and additional student details

--- src/app/Components/view-student/view-student.ts
+++ src/app/Components/view-student/view-student.ts
@@ -1,51 +1,65 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StudentResDto } from '../../Interface/studentResDto';
 import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { StudentService } from '../../Services/Admin/student-service';
+import { FormsModule } from '@angular/forms';
 
-
+// ✅ Move type here, outside the class
+export type StudentField = {
+  label: string;
+  key: keyof StudentResDto;
+  type?: string;
+};
 
 @Component({
+  standalone: true,
   selector: 'app-view-student',
-  imports: [],
   templateUrl: './view-student.html',
-  styleUrl: './view-student.css'
+  styleUrl: './view-student.css',
+  imports: [CommonModule, FormsModule],
 })
 export class ViewStudent implements OnInit {
-  student!: StudentResDto; // not an array
+  student!: StudentResDto; 
+  isEditing = false;
+  studentId!: number;
+  profilePreview: string | ArrayBuffer | null = null;
 
-
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private location: Location,
     private studentService: StudentService
-  ) { }
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.studentId = +params['stud']; 
+      this.fetchStudent(this.studentId);
+    });
+  }
 
   goBack(): void {
     this.location.back();
   }
 
-  studentId!: number;
-
-  ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      this.studentId = +params['stud']; // convert to number
-      console.log('Student ID:', this.studentId);
-      this.fetchStudent(this.studentId)
-    });
-  }
-
-
   fetchStudent(id: number) {
     this.studentService.getFetchStudentById(id).subscribe({
       next: (value) => {
-        this.student = value; 
-        console.log("student by id",this.student);
+        this.student = value;
       },
       error: (error) => {
         console.error('Error fetching student:', error);
       }
     });
+  }
+
+  onImageChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => (this.profilePreview = reader.result);
+      reader.readAsDataURL(file);
+    }
   }
 
 }
